e558b49dc965198286d1f927ed83a3ba1222636e
Julija Jelicanin <julijajelicanin@192.168.1.8>
2023-03-29 15:30:15 +0200

    Homepage & RecipeList

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,9 +1,21 @@
-import './Home.css'
-
 import React from 'react'
 
+//styles
+import './Home.css'
+
+//components
+import RecipeList from '../../components/RecipeList'
+import { useFetch } from '../../hooks/useFetch'
+
 export default function Home() {
+
+    const {data, isPending, error}=useFetch('http://localhost:3004/recipes')
+
   return (
-    <div>Home</div>
+    <div className='home'>
+        {error && <p className='error'>{error}</p>}
+        {isPending && <p className='loading'>Loading...</p>}
+        {data && <RecipeList recipes={data}/>}
+    </div>
   )
 }
